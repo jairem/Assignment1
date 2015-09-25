002de91aa64db0950af354beefd058e5aa6b8f74
Jair <jchepetla@gmail.com>
2015-09-24 18:13:33 -0700

    Assignment 1

---
+++ script.js
@@ -0,0 +1,191 @@
+/*
+//STEP 1
+var someMonth;
+function currentMonth;
+const CURRENTMONTH;
+var summerMonths;
+function myLibraryFunction;
+*/
+
+
+/*
+//STEP 2
+5 //Numeric literal
+"Welcome" //String literal
+True //Boolean literal
+null //Null literal
+*/
+
+
+/*
+//STEP 3
+var theFirstExpression = 8 + (4 * 2);
+var theSecondExpression = theFirstExpression + 3;
+*/
+
+
+/*
+//STEP 4
+var sFirstName, sLastName, sAddress, sCity, sState, nZipCode, nYourAge, sReferralSource, bMayWeContactYou;
+*/
+
+
+/*
+//STEP 5
+var sFirstName;
+var sLastName;
+var sAddress;
+var sCity;
+var sState;
+var nZipCode;
+var nYourAge;
+var sReferralSource; 
+var bMayWeContactYou;
+sFirstName = "Jair";
+sLastName = "Chepetla";
+sAddress = "101 W. Broadway";
+sCity = "San Diego"
+sState = "CA"
+nZipCode = 92105;
+nYourAge = 29;
+sReferralSource = "jobs.com";
+bMayWeContactYou = true;
+
+var sFirstName = "Jair";
+var sLastName = "Chepetla";
+var sAddress = "101 W. Broadway";
+var sCity = "San Diego"
+var sState = "CA"
+var nZipCode = 92105;
+var nYourAge = 29;
+var sReferralSource = "jobs.com";
+var bMayWeContactYou = true;
+
+var sFirstName = "Jair", sLastName = "Chepetla", sAddress = "101 W. Broadway", sCity = "San Diego", sState = "CA", nZipCode = 92105, nYourAge = 29, sReferralSource = "jobs.com", bMayWeContactYou = true;
+*/
+
+/*
+//STEP 6
+var firstExample = 7 + " seas";
+console.log(firstExample);
+var secondExample = true + " friends";
+console.log(secondExample);
+var thirdExample = 6 + true;
+console.log(thirdExample);
+*/
+
+
+/*
+//STEP 7
+var guitarBrand = "Fender";
+console.log("I play a  " + guitarBrand + " guitar on a "  + ampBrand + " amplifier.");
+var ampBrand = "Vox"
+*/
+
+
+/*
+//STEP 8
+//It is not valid, the apostrophe in (I'm) needs to be escaped like this:
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+*/
+
+/*
+//STEP 9
+var theNullVariable = null;
+console.log(theNullVariable);
+var theUndefinedVariable;
+console.log(theUndefinedVariable);
+*/
+
+
+/*
+//STEP 10
+console.log(typeof "Me", typeof 7, typeof false, typeof {name:'Jair', age:29}, typeof lastName);
+*/
+
+
+/*
+//STEP 11
+alert("Hello " + "Jair Chepetla" + ", welcome to the JavaScript class!");
+*/
+
+/*
+//STEP 12
+var name = "Jair Chepetla";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+/*
+//STEP 13
+var name = "Jair Chepetla";
+var course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+/*
+//STEP 14
+var name = "Jair Chepetla";
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+/*
+//STEP 15
+var userName = prompt("What is your name?");
+alert(userName);
+*/
+
+
+/*
+//STEP 16
+var userClass = prompt("What class are you taking?");
+alert(userClass);
+*/
+
+/*
+//STEP 17
+var x = 10;
+var y = 20;
+console.log(x + y);
+*/
+
+/*
+//STEP 18
+var x = 20;
+x += 20;
+console.log(x);
+*/
+
+/*
+//STEP 19
+var x = 20;
+x *=5;
+console.log(x);
+*/
+
+/*
+//STEP 20
+var x = 20 % 3;
+x /= 1;
+console.log(x);
+*/
+
+/*
+//STEP 21
+var x = 10;
+var y = 5;
+console.log(y < x);
+*/
+
+/*
+//STEP 22
+var a = 99;
+var b = 9;
+console.log(b > a);
+*/
+
+
+
+
+
